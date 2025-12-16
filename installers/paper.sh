88b1438cbba265ff6f469dcecd5366b237c9e1cd
Luca-rickrolled-himself <88965309+LucaBarbaLata@users.noreply.github.com>
2025-12-16 08:39:15 +0200

    Update paper.sh
    Added option to prompt the user for how much RAM should be allocated to the server

--- installers/paper.sh
+++ installers/paper.sh
@@ -58,6 +58,11 @@ MC_VERSION=${MC_VERSION:-1.21.10}
 read -p "Enter the build number (default: 117): " BUILD_NUMBER
 BUILD_NUMBER=${BUILD_NUMBER:-117}
 
+# Ask the user for RAM allocation
+read -p "Enter the amount of RAM to allocate in GB (default: 3): " RAM_GB
+RAM_GB=${RAM_GB:-3}
+RAM_MB=$((RAM_GB * 1024))
+
 # Define the download URL
 JAR_URL="https://api.papermc.io/v2/projects/paper/versions/$MC_VERSION/builds/$BUILD_NUMBER/downloads/paper-$MC_VERSION-$BUILD_NUMBER.jar"
 
@@ -87,7 +92,7 @@ fi
 log "${CYAN}[✍️] Creating start script..."
 cat <<EOF > start.sh
 #!/bin/bash
-java -Xms3072M -Xmx3072M --add-modules=jdk.incubator.vector -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -jar server.jar --nogui
+java -Xms${RAM_MB}M -Xmx${RAM_MB}M --add-modules=jdk.incubator.vector -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -jar server.jar --nogui
 EOF
 
 # Give execution permission to start script
